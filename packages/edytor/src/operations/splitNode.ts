f42a03eec1d798198c8e134ec57db92f5f44feff
Arnaud DERBEY <arnaud@derbey.dev>
2021-11-14 17:36:07 +0100

    comment split node / fix edge split with children

--- packages/edytor/src/operations/splitNode.ts
+++ packages/edytor/src/operations/splitNode.ts
@@ -9,8 +9,6 @@ import {
   leafString,
   leafLength,
   deleteLeafText,
-  leafNode,
-  getIndex,
   leafNodeContentLength,
   hasChildren,
   getNodeChildren,
@@ -24,40 +22,50 @@ export type splitNodeOperation = {
   yText?: YText;
 };
 
-export const splitNode = (editor: Editor) => {
-  const { start, end, type, length, setPosition, edges } = editor.selection;
-
-  const leaf = start.leaf;
+const split = (editor: Editor) => {
+  const { start, setPosition } = editor.selection;
   const leafContent = leafNodeContent(start.leaf);
-  const node = leafNode(start.leaf);
 
-  const indexOfLeaf = getIndex(start.leaf);
-  const indexOfNode = getIndex(node);
+  // we get the next leaves + right text that represent the split content to move below
+  const nextLeaves = leafContent.toArray().filter((_, i) => i > start.leafIndex);
+  const rightText = leafString(start.leaf).substring(start?.offset, leafLength(start.leaf));
 
-  const split = () => {
-    const nextLeaves = leafContent.toArray().filter((_, i) => i > indexOfLeaf);
+  // we delete the right text from the current leaf
+  deleteLeafText(start.leaf, start?.offset, rightText.length, false);
+  // we get the container to push split content into
+  const newParent = hasChildren(start.node) ? getNodeChildren(start.node) : (start.node.parent as YArray<any>);
+  // we create the new leaf with the right text
+  const newLeaf = createLeaf({ ...start.leaf.toJSON(), id: undefined, text: rightText });
+  // we insert the new leaf and the next leaves into their new parent
+  newParent.insert(hasChildren(start.node) ? 0 : start.nodeIndex + 1, [
+    createNode(editor.defaultBlock, {
+      children: [],
+      content: [newLeaf].concat(nextLeaves.map((leaf) => createLeaf(leaf.toJSON())))
+    })
+  ]);
+  setPosition(newLeaf.get("id") as string, { offset: 0 });
+  // we removes the next leaves from their original parent
+  leafContent.delete(start.leafIndex + 1, leafNodeContentLength(start.leaf) - start.leafIndex - 1);
+};
 
-    const rightText = leafString(start.leaf).substring(start?.offset, leafLength(start.leaf));
-    deleteLeafText(start.leaf, start?.offset, rightText.length, false);
+export const splitNode = (editor: Editor) => {
+  const { start, type, setPosition, edges } = editor.selection;
 
-    const newParent = hasChildren(node) ? getNodeChildren(node) : (node.parent as YArray<any>);
-    const newLeaf = createLeaf({ ...leaf.toJSON(), id: undefined, text: rightText });
-    newParent.insert(hasChildren(node) ? 0 : indexOfNode + 1, [
-      createNode(editor.defaultBlock, {
-        children: [],
-        content: [newLeaf].concat(nextLeaves.map((leaf) => createLeaf(leaf.toJSON())))
-      })
-    ]);
-    setPosition(newLeaf.get("id") as string, { offset: 0 });
-    leafContent.delete(indexOfLeaf + 1, leafNodeContentLength(start.leaf) - indexOfLeaf - 1);
-  };
-
+  // only split if the cursor isn't edgy and on a single node
   let doSplit = (!edges.startNode || !edges.endNode) && type !== "multinodes";
 
   if ((edges.endNode || edges.startNode) && type === "collapsed") {
     // if cursor is at edge node with insert a default block before of after the current node
     const newNode = createNode(editor.defaultBlock);
-    (node.parent as YArray<any>).insert(indexOfNode + (edges.endNode ? 1 : 0), [newNode]);
+    console.log(hasChildren(start.node) && edges.endNode);
+
+    if (hasChildren(start.node) && edges.endNode) {
+      // if node has children we insert the new node before them
+      getNodeChildren(start.node).insert(0, [newNode]);
+    } else {
+      // if node don't have children we insert the new node after or before it
+      (start.node.parent as YArray<any>).insert(start.nodeIndex + (edges.endNode ? 1 : 0), [newNode]);
+    }
     return setTimeout(() => {
       setPosition(
         newNode
@@ -71,21 +79,21 @@ export const splitNode = (editor: Editor) => {
 
   switch (type) {
     case "collapsed": {
-      doSplit && split();
+      doSplit && split(editor);
       break;
     }
     case "singlenode": {
       deleteText(editor, { mode: "backward" });
-      doSplit && split();
-      !doSplit && setPosition(start.leaf.get("id"), { offset: start.offset });
+      doSplit && split(editor);
+      !doSplit && setPosition(start.leafId, { offset: start.offset });
 
       break;
     }
     case "multileaves":
     case "multinodes": {
       deleteText(editor, { mode: "backward" });
-      doSplit && split();
-      !doSplit && setPosition(start.leaf.get("id"), { offset: start.offset });
+      doSplit && split(editor);
+      !doSplit && setPosition(start.leafId, { offset: start.offset });
     }
   }
 };
